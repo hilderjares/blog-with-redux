773502677e23ad27c97bcc7e623156ef3a304717
hilderjares <hider1195@gmail.com>
2020-02-25 18:15:31 -0300

    export action

--- src/blog/actions/postActions.js
+++ src/blog/actions/postActions.js
@@ -1,11 +1,9 @@
 import { v4 as uuidv4 } from "uuid";
 
-const add = ({ name, tags, post }) => ({
+export const addPost = ({ name, tags, post }) => ({
     type: "ADD_POST",
     id: uuidv4(),
     name: name,
     tags: tags,
     post: post,
 });
-
-export { add };
